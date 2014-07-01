62e72d1f5feb3377816cda104381452161594852
Quinn McHenry <quinn.mchenry@smallplanet.com>
2014-07-01 17:35:29 -0400

    WIP exploration on SimpleTypes with Swift

--- Examples/SwiftPlanets/SwiftPlanets/Gaxb.swift
+++ Examples/SwiftPlanets/SwiftPlanets/Gaxb.swift
@@ -3,6 +3,8 @@
 //  Copyright (c) 2014 Small Planet. All rights reserved.
 //
 
+import UIKit
+
 protocol GaxbElement {
     var parent: GaxbElement? { get }
     var xmlns: String { get }
@@ -12,4 +14,38 @@ protocol GaxbElement {
     func sequencesXML(useOriginalValues:Bool) -> String
     func toXML(useOriginalValues:Bool) -> String
     func toXML() -> String
+}
+
+protocol GaxbType {
+    init(GaxbString withGaxbString: String)
+    mutating func setWithGaxbString(GaxbString: String)
+    func toGaxbString() -> String
+}
+
+extension CGRect: GaxbType {
+    init(GaxbString withGaxbString: String) {
+        let (origin, size) = CGRect.componentsFromString(withGaxbString)
+        self.init(origin: origin, size: size)
+    }
+    mutating func setWithGaxbString(GaxbString: String) {
+        var (newOrigin, newSize) = CGRect.componentsFromString(GaxbString)
+        origin = newOrigin
+        size = newSize
+    }
+    func toGaxbString() -> String {
+        return "\(origin.x),\(origin.y),\(size.width),\(size.height)"
+    }
+    static func componentsFromString(string: String) -> (CGPoint, CGSize) {
+        var x=0, y=0, w=0, h=0
+        var components = string.componentsSeparatedByString(",")
+        if components.count == 4 {
+            x = components[0].toInt()!
+            y = components[1].toInt()!
+            w = components[2].toInt()!
+            h = components[3].toInt()!
+        }
+        let origin = CGPoint(x: x, y: y)
+        let size = CGSize(width: w, height: h)
+        return (origin, size)
+    }
 }
